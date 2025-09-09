78a11dcd44960350dab221c321b0b0730160ba6e
Ritej44 <ritejbouaicha@gmail.com>
2025-09-09 17:20:55 +0100

    domain

--- src/main/java/com/example/WSM/config/SecurityConfig.java
+++ src/main/java/com/example/WSM/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig  implements WebMvcConfigurer {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://wsm-kappa-ebon.vercel.app"));
+        configuration.setAllowedOrigins(List.of("https://angular-gray-mu.vercel.app/login"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
